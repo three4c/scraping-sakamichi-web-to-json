fab5e5953e3f01d2334da7b864e7180515783942
three4c <three4c@gmail.com>
2023-06-17 15:33:04 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
@@ -25,7 +25,6 @@ import {
   ConvertDataType,
   ArgsType,
   TicketType,
-  ColorType,
   MemberScheduleType,
 } from 'types';
 
